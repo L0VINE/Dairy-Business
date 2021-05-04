3c2b58a725380d66e2fc598d4f9250415547b281
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-05-04 13:38:34 +0300

    modified main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const inputLabel = document.querySelectorAll('.form__label');
+const inputForm = document.querySelectorAll('.form__input');
+const btn = document.querySelector('.form__btn');
+const container = document.querySelector('.container')
+const reports = document.querySelector('.reports')
+
+const inputShedA = document.querySelector('.form__input--shedA');
+const inputShedB = document.querySelector('.form__input--shedB');
+const inputShedC = document.querySelector('.form__input--shedC');
+const inputShedD = document.querySelector('.form__input--shedD');
+
+const dailyReport = document.querySelector('.report--daily');
+const weeklyReport = document.querySelector('.report--weekly');
+const monthlyReport = document.querySelector('.report--monthly');
+const yearlyReport = document.querySelector('.report--yearly');
