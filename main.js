8e9476376052d2fa103485fc1925cf95ea3c7df8
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-05-04 13:39:51 +0300

    modified main.js

--- main.js
+++ main.js
@@ -15,3 +15,13 @@ const dailyReport = document.querySelector('.report--daily');
 const weeklyReport = document.querySelector('.report--weekly');
 const monthlyReport = document.querySelector('.report--monthly');
 const yearlyReport = document.querySelector('.report--yearly');
+const err = () => alert('input must be positive numbers');
+
+const getDaysInMonth = function (month, year) {
+  const days = new Date(year, month, 0).getDate();
+  return days;
+};
+
+class App {
+  #sellingPrice = 45;
+  #shedMovements = [];
