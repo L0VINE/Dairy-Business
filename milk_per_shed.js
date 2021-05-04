0b0f4a7cab72de446187217e97de8d94f843e8b8
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-05-04 13:04:03 +0300

    modified index.html

---
+++ milk_per_shed.js
@@ -0,0 +1,9 @@
+// var weight_in_lbs = prompt("Please enter the weight in lbs you wish to convert:");
+// var pound_to_kg = (weight_in_lbs * .45359237);
+// console.log("The weight converted in kg is" + " " + pound_to_kg.toFixed(3))
+// var annualSales = prompt("What is the projected total sales amount?");
+// var salesPrediction = (annualSales * .23);
+// console.log("Your annual profit is " + salesPrediction.toFixed(2));
+// const shedA=Document.getElementById("shedA").innerHTML= prompt("Please enter the weight in lbs you wish to convert:");
+const number = prompt('input')
+console.log(number)
